870baeb2d0b5aa07dba33e615e49d89305dbc1a8
chimptime <admin@skylight.digital>
2017-03-28 15:53:43 -0400

    lots of refactoring...added brand colors and assets..and more

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
+
+$(function() {
+  $('#main-nav a.current').removeClass('current')
+  $('a[href="' + location.pathname + '"]').closest('a').addClass('current')
+})
